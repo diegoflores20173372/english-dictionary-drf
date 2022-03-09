5ba69330dabf5f6c435dded7c7b2b2fd242051b4
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-03-08 21:17:06 -0500

    "CRUD and Refactor the URL's"

--- english_api/urls.py
+++ english_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import WordList
+# from .api_views import WordList
+from . import generic_views
 
 urlpatterns = [
-    path('words/', WordList.as_view(), name="word")
+    # path('words/', WordList.as_view(), name="word")
+    path('words/', generic_views.ListCreateWord.as_view(), name='word_list'),
+    path('words/<int:pk>', generic_views.RetrieveUpdateDestroyWord.as_view(), name='word_detail'),
 ]
