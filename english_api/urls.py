533c027304da08eb51e14801dadf1f3e8fee4cf0
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-03-08 23:04:43 -0500

    "Using ViewSets Personalized to Control the options from the CRUD"

--- english_api/urls.py
+++ english_api/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 # from .api_views import WordList
-from . import generic_views
+# from . import generic_views
+from . import mixins
+
+from rest_framework import routers
+from . import viewsets
+
+router = routers.DefaultRouter()
+# router.register('english_api', viewsets.WordViewSet)
+router.register('english_api', mixins.WordViewSet)
 
 urlpatterns = [
     # path('words/', WordList.as_view(), name="word")
-    path('', generic_views.ListCreateWord.as_view(), name='word_list'),
-    path('/<int:pk>', generic_views.RetrieveUpdateDestroyWord.as_view(), name='word_detail'),
+    # path('', generic_views.ListCreateWord.as_view(), name='word_list'),
+    # path('/<int:pk>', generic_views.RetrieveUpdateDestroyWord.as_view(), name='word_detail'),
 ]
+
+urlpatterns += router.urls
